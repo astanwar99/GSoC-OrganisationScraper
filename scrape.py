125cf511d5e27505448a0156b51930aecb036de8
astanwar99 <as.tanwar3198@gmail.com>
2019-03-24 15:39:44 +0530

    Did some cleaning.

--- scrape.py
+++ scrape.py
@@ -1,42 +1,26 @@
 #!/usr/bin/env python
 import requests
-import sys
 import warnings
-import signal
 from bs4 import BeautifulSoup
+import json
+from flask import Flask, request
 
-url = "https://summerofcode.withgoogle.com/archive/2018/organizations/"
-default = "https://summerofcode.withgoogle.com"
+app = Flask(__name__)
 
 #To avoid warning messages 
 warnings.filterwarnings("ignore")
 
-#Control flow of control through 'Signal' from user.
-def signal_handler(signal, frame):
-    confirmation = raw_input("Really want to exit (y/n)? ") 
-    confirmation.replace(" ", "")
-    confirmation = confirmation.lower()
-    if confirmation == "y" or confirmation == "yes":
-        sys.exit(0)
-    else:
-        return
-
-signal.signal(signal.SIGINT, signal_handler)
-
 #Main function.
+@app.route('/genData')
 def scrape():
-    if(len(sys.argv) == 2):
-        user_pref = sys.argv[1]
-    else:
-        user_pref = raw_input("Enter a technology of preference: " )
-   
-    user_pref = user_pref.lower()
-    user_pref.replace(" ", "")
-    count = 0
+    url = "https://summerofcode.withgoogle.com/archive/2018/organizations/"
+    default = "https://summerofcode.withgoogle.com"
 
+    genData_list = []
+    count = 0
     response = requests.get(url)
     html = response.content
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, 'lxml')
     orgs = soup.findAll('li', attrs={'class': 'organization-card__container'})
 
     for org in orgs:
@@ -50,30 +34,34 @@ def scrape():
                 'class': 'organization__tag organization__tag--technology'
             }
         )
+        tech = []
+        for tag in tags:
+            tech.append(tag.text)
+        
         description_element = soup.find('div', attrs={'class': 'org__long-description'})
         description = description_element.p.text
 
         mdButton = soup.findAll('md-button', attrs={'class': 'md-primary org__meta-button'})
-
         contact = "No contact info available"
         for link in mdButton:
             if hasattr(link, 'href'):
                 if 'mailto:' in link['href']:
                     contact = link['href']
-            
+                
+        output_dict = {
+            "organization" : org_name,
+            "link" : org_link,
+            "description" : description,
+            "technologies" : tech,
+            "contact" : contact
+        }
+        genData_list.append(output_dict)
+        count += 1
+        print('Page: ',count)
+        #Details for 10 organization - Can be changed for more or less.
+        if count == 10:
+            return json.dumps(genData_list)
 
-        print ("Name: " + org_name)
-        print ("Link: " + org_link)
-        print ("Tag: ")
-        print ("Description: " + description)
-        print (contact)
-        for tag in tags:
-            print (", " + tag.text)
-            count += 1
 
-    if count == 0:
-        print ("Enter a valid technology name.")
-
-#Calling scrape().
-if __name__ == "__main__":
-    scrape()
+if __name__ == '__main__':
+    app.run(debug=True)
